c83fbd76a27dfce3d5015128f6210c6d165f98e4
alejandropch <alejandropachas1@gmail.com>
2022-02-04 15:48:20 -0500

    basic fade in added at the navbar compoenent depending of the user position

--- src/components/NavBar/Nav.Bar.tsx
+++ src/components/NavBar/Nav.Bar.tsx
@@ -1,30 +1,35 @@
-import * as React from 'react'
+import React,{ useEffect, useState }from 'react'
 import { Link } from 'react-router-dom'
 import {Nav} from './styles'
 export default function NavBar() {
+    const [fixed, setFixed]=useState(false)
+
+    function onScroll():void {
+        const showFixed = window.scrollY > 100
+        fixed != showFixed && setFixed(showFixed)
+        console.log(fixed);
+    }
+
+    useEffect(()=>{
+        document.addEventListener('scroll',onScroll)
+        return ()=> document.removeEventListener('scroll',onScroll)
+    },[fixed])
+
+
     return (
-            <Nav className="navbar navbar-expand">
-{/*                 
-                <div className="container-fluid nav nav-tabs justify-content-end flex-nowrap"> */}
-
-                 <ul className="container-fluid nav justify-content-center flex-nowrap">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">About</Link>
-                        </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">
-                        Projects
-                        </Link>
-                        </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">
-                        Contact
-                        </Link></li>
-
-                 </ul>
-
-
-            {/* </div> */}
+        <Nav className="navbar navbar-expand" fixed={fixed}>
+        {/* <div className="container-fluid nav nav-tabs justify-content-end flex-nowrap"> */}
+            <ul className="container-fluid nav justify-content-center flex-nowrap">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Projects</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Contact</Link>
+                </li>
+            </ul>
         </Nav>
 
     )
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,11 +1,32 @@
-import styled from "styled-components";
+import styled,{css} from "styled-components";
 
-const Nav= styled.nav`
+interface Custom {
+    readonly fixed:boolean
+}
+
+const Nav= styled.nav<Custom>`
     position: absolute;
     flex-wrap: nowrap;
     display: flex;
     left: 0;
     right: 0;
     top: 0;
+
+    ${ props => props.fixed && css`
+        position: fixed;
+        animation fade-in 1s;
+        z-index:1;
+    `}
+
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+    
 `
 export {Nav}
