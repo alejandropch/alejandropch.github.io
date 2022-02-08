d9fab5f6eb5303a5a88bea0493444c1460ac0ba6
alejandropch <alejandropachas1@gmail.com>
2022-02-07 22:26:43 -0500

    slide button component added

---
+++ src/components/SlideButton/Slide.Button.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import useMaxPages from '../../hooks/useMaxPages'
+
+interface Params{
+    readonly page:number,
+         setPage(x:number):void
+}
+export default function SlideButton({page, setPage}:Params) {
+    const maxPages = useMaxPages()
+  
+    function handleClick (direct:string) {
+        
+        if(direct == "right"){
+            return setPage( page + 1 )
+        }
+        if(direct == "left"){
+            page -1 == 0 ? setPage(maxPages) : setPage( page - 1 )
+        }
+    }
+
+  return (
+    <>
+        <div className="carousel-indicators">
+        {
+            [...Array(maxPages)].map((e,key)=> {
+            const pageNumber = key + 1
+            const isActive = (pageNumber==page) && true
+
+            return <button key= {key} type="button" data-bs-target="#Target" data-bs-slide-to={pageNumber} className={isActive?"active":""} aria-label={`Slide ${pageNumber}`}></button>
+            })
+        }
+        </div>
+
+        <button className="carousel-control-prev" onClick={()=>handleClick('left')} type="button" data-bs-target="#Target" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" onClick={()=>handleClick('right')} type="button" data-bs-target="#Target" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+        </button>  
+    </>
+    )
+}
