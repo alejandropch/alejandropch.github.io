ac9f5fe7a97cce579f82263c632c1dfb4ca724bf
alejandropch <alejandropachas1@gmail.com>
2022-02-07 22:22:30 -0500

    changing this function and file name

--- src/components/ProjectsList/Projects.List.tsx
+++ src/components/ProjectsList/Projects.List.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ProyectCard from '../ProyectCard/Proyect.Card';
 import axios from '../../http-common'
-import useMaxPages from '../../hooks/useMaxPages'
-
+import SlideButton from '../SlideButton/Slide.Button'
 // import Rows  from './styles.js';
 export default function Proyects() {
 
     const [projects, setProjects] = useState([])
     const [page, setPage] = useState(1)
-    const maxPages = useMaxPages()
     const http = axios()
    
     useEffect(() => {
@@ -22,25 +20,15 @@ export default function Proyects() {
         fetchData()
     },[page])
 
-    function handleClick (direct:string) {
-        
-        if(direct == "right"){
-            return setPage( page + 1 )
-        }
-        if(direct == "left"){
-            page -1 == 0 ? setPage(maxPages) : setPage( page - 1 )
-        }
-    }
 // if the key is superior to the number of projects you should get, 
   return (
-    <div className="container">
+    <div className="container position-relative">
+    <SlideButton page={page} setPage={setPage}/>
         <div className="row gy-5 row-cols-1 row-cols-md-2 ">
           {
             projects.map((item,key) => <ProyectCard key={key} data={item} isOdd={(key%2 == 0)} />)
           }
-          <button onClick={()=>handleClick('left')}>{'<'}</button>
-          <button onClick={()=>handleClick('right')}>{'>'}</button>
-
+     
         </div>
     </div>
   );
