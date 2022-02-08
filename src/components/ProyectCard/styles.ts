0cbb150e5a45c3bfeadead7d993d81004605c9f8
alejandropch <alejandropachas1@gmail.com>
2022-02-08 13:14:03 -0500

    getting rid of unwanted styles

--- src/components/ProyectCard/styles.ts
+++ src/components/ProyectCard/styles.ts
@@ -18,34 +18,24 @@ const Card = styled.div<Custom>`
 
     position:relative;
     ${props => props.show && props.isOdd && css`
-    -webkit-animation-name: rotateInDownLeft;
-            animation-name: rotateInDownLeft;
-            visibility: visible;
-            -webkit-animation-duration: 1s;
-            animation-duration: 1s;
-            -webkit-animation-fill-mode: both;
-            animation-fill-mode: both;
-            `}
+        animation-name: rotateInDownLeft;
+        visibility: visible;
+        animation-duration: 1s;
+        animation-fill-mode: both;
+    `}
     ${props => props.show && !props.isOdd && css`
 
-            -webkit-animation-name: rotateInDownRight;
-            animation-name: rotateInDownRight;
-            visibility: visible;
-            -webkit-animation-duration: 1s;
-            animation-duration: 1s;
-            -webkit-animation-fill-mode: both;
-            animation-fill-mode: both;
-            `}
-     ${props => props.theme == "text" && css`
-        -webkit-backface-visibility: visible !important;
-        backface-visibility: visible !important;
-        -webkit-animation-name: flipInX;
-        animation-name: flipInX;
-        -webkit-animation-duration: 1s;
+        animation-name: rotateInDownRight;
+        visibility: visible;
         animation-duration: 1s;
-        -webkit-animation-fill-mode: both;
         animation-fill-mode: both;
-      
+    `}
+     ${props => props.theme == "text" && css`
+        backface-visibility: visible !important;
+        animation-name: flipInX;
+        animation-duration: 1s;
+        animation-fill-mode: both;
+
         color:#fff;
         background:#000;
       `}
@@ -64,60 +54,45 @@ const Card = styled.div<Custom>`
     } 
     @keyframes flipInX {
         0% {
-        -webkit-transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
         transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
-        -webkit-transition-timing-function: ease-in;
         transition-timing-function: ease-in;
         opacity: 0;
         }
         40% {
-        -webkit-transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
         transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
-        -webkit-transition-timing-function: ease-in;
         transition-timing-function: ease-in;
         }
         60% {
-        -webkit-transform: perspective(400px) rotate3d(1, 0, 0, 10deg);
         transform: perspective(400px) rotate3d(1, 0, 0, 10deg);
         opacity: 1;
         }
         80% {
-        -webkit-transform: perspective(400px) rotate3d(1, 0, 0, -5deg);
         transform: perspective(400px) rotate3d(1, 0, 0, -5deg);
         }
         100% {
-        -webkit-transform: perspective(400px);
         transform: perspective(400px);
         }
     }
     @keyframes rotateInDownLeft {
         0% {
-        -webkit-transform-origin: left bottom;
         transform-origin: left bottom;
-        -webkit-transform: rotate3d(0, 0, 1, -45deg);
         transform: rotate3d(0, 0, 1, -45deg);
         opacity: 0;
         }
         100% {
-        -webkit-transform-origin: left bottom;
         transform-origin: left bottom;
-        -webkit-transform: none;
         transform: none;
         opacity: 1;
         }
     } 
     @keyframes rotateInDownRight {
         0% {
-        -webkit-transform-origin: right bottom;
         transform-origin: right bottom;
-        -webkit-transform: rotate3d(0, 0, 1, 45deg);
         transform: rotate3d(0, 0, 1, 45deg);
         opacity: 0;
         }
         100% {
-        -webkit-transform-origin: right bottom;
         transform-origin: right bottom;
-        -webkit-transform: none;
         transform: none;
         opacity: 1;
         }
