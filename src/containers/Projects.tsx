11f601dc44b43aca4de11f44adc1a06e15cac579
alejandropch <alejandropachas1@gmail.com>
2022-02-08 22:39:22 -0500

    Projects container created

---
+++ src/containers/Projects.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef, useState } from 'react';
+import SlideButton from '../components/SlideButton/Slide.Button'
+import axios from '../http-common'
+import ProjectsList from '../components/ProjectsList/Projects.List'
+export default function Projects() {
+    const [page, setPage] = useState(1)
+    const [projects, setProjects] = useState([])
+    const http = axios()
+    useEffect(() => {
+        const fetchData = async () =>{
+            const { data } = await http.get('/projects',{params:{page}});
+            if(!data.length){
+                return setPage(1)
+            }
+            setProjects(data);
+        }
+        fetchData()
+        return()=>setProjects([]);
+    },[page])
+    console.log(projects);
+    
+  return (
+    <div className="container position-relative">
+        <SlideButton page={page} setPage={setPage}/>
+        <ProjectsList projects={projects} />
+    </div>
+
+  )
+}
